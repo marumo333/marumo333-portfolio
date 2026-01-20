65a072660790841ed7069bb6b3461397fdb96ce4
marumo333 <seito5026junior@icloud.com>
2026-01-20 18:07:46 +0900

    feature: ruffによるpythonコード整形を実現

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from uuid import UUID
+
 from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
+
 
 class ProfileResponse(BaseModel):
     id: UUID
@@ -12,6 +14,6 @@ class ProfileResponse(BaseModel):
 
     model_config = ConfigDict(
         alias_generator=to_camel,
-        populate_by_name=True, 
+        populate_by_name=True,
         from_attributes=True
     )
