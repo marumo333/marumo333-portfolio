08d346e075cdb931de6b253cc38aab3399fe34d5
marumo333 <seito5026junior@icloud.com>
2026-01-22 00:22:37 +0900

    style: レスポンシブに対応

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    //ホーム画面は静的なため、記述なし
+	//ホーム画面は静的なため、記述なし
 </script>
 
-   <section class="text-center max-w-2xl"> 
-      <h1 class="text-4xl md:text-4xl lg:text-5xl font-bold text-primary mb-4">
-          marumo333's Portfolio
-      </h1>
-      
-      <p class="text-base md:text-lg lg:text-xl text-gray-700 leading-relaxed">    
-        こちらはmarumo333のスキル・経歴・開発実績が参照できるportfolioのwebアプリです。
-        ログインを行うことで、RAGを用いたportfolio記載の内容に限定したchatが可能です。
-      </p>
-   </section>
+<section class="max-w-2xl text-center">
+	<h1 class="mb-4 text-4xl font-bold text-primary md:text-4xl lg:text-5xl">
+		marumo333's Portfolio
+	</h1>
+
+	<p class="text-base leading-relaxed text-gray-700 md:text-lg lg:text-xl">
+		こちらはmarumo333のスキル・経歴・開発実績が参照できるportfolioのwebアプリです。
+		ログインを行うことで、RAGを用いたportfolio記載の内容に限定したchatが可能です。
+	</p>
+</section>
