3eeb02cac8d4a83770faea27ab0c3eae81a71b6a
marumo333 <seito5026junior@icloud.com>
2026-01-22 00:03:57 +0900

    feature: Home画面を追加

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    //ホーム画面は静的なため、記述なし
+</script>
+
+   <section class="text-center max-w-2xl"> 
+      <h1 class="text-4xl md:text-4xl lg:text-5xl font-bold text-primary mb-4">
+          marumo333's Portfolio
+      </h1>
+      
+      <p class="text-base md:text-lg lg:text-xl text-gray-700 leading-relaxed">    
+        こちらはmarumo333のスキル・経歴・開発実績が参照できるportfolioのwebアプリです。
+        ログインを行うことで、RAGを用いたportfolio記載の内容に限定したchatが可能です。
+      </p>
+   </section>
